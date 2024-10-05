94ea5d7b713b7b416d13e423e2fb1a2e21f4de8a
More-Pe <peraltamorena.va@gmail.com>
2024-10-05 11:59:15 +0200

    feat: add access seervices

--- src/services/AccessServices.ts
+++ src/services/AccessServices.ts
@@ -1,3 +1,50 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:4000/api';
+const URL = 'http://localhost:4000/api/access';
+
+export const registerEntry = async (room_id: number, token: string) => {
+    try {
+        const response = await axios.post(
+            `${URL}/entry`, 
+            { room_id }, 
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+        return response.data;
+    } catch (error: any) {
+        throw new Error(error.response?.data?.message || 'Error registering entry');
+    }
+};
+
+export const registerExit = async (room_id: number, token: string) => {
+    try {
+        const response = await axios.post(
+            `${URL}/exit`, 
+            { room_id }, 
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+        return response.data;
+    } catch (error: any) {
+        throw new Error(error.response?.data?.message || 'Error registering exit');
+    }
+};
+
+export const getCurrentPeopleInRoom = async (room_id: number, token: string) => {
+    try {
+        const response = await axios.get(`${URL}/current/room/${room_id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        return response.data;
+    } catch (error: any) {
+        throw new Error(error.response?.data?.message || 'Error getting current people in room');
+    }
+};
