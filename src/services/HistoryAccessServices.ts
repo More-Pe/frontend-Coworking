c92ee12e56dd3d5b256d428c20afe3ffd1002950
More-Pe <peraltamorena.va@gmail.com>
2024-10-05 12:02:31 +0200

    feat: add access histories services

--- src/services/HistoryAccessServices.ts
+++ src/services/HistoryAccessServices.ts
@@ -1,3 +1,31 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:4000/api';
+const URL = 'http://localhost:4000/api/access-histories';
+
+export const getAccessHistories = async (start_date: string, end_date: string, token: string) => {
+    try {
+        const response = await axios.get(`${URL}`, {
+            params: { start_date, end_date },
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        return response.data;
+    } catch (error: any) {
+        throw new Error(error.response?.data?.message || 'Error retrieving access histories');
+    }
+};
+
+export const getAccessHistoriesByRoom = async (room_id: number, start_date: string, end_date: string, token: string) => {
+    try {
+        const response = await axios.get(`${URL}/room/${room_id}`, {
+            params: { start_date, end_date },
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        return response.data;
+    } catch (error: any) {
+        throw new Error(error.response?.data?.message || 'Error retrieving access histories for the room');
+    }
+};
