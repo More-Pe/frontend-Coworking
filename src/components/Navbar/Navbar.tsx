dbd8770204df98f2f075f5cb706093e5df5dd4e8
More-Pe <peraltamorena.va@gmail.com>
2024-10-13 13:46:59 +0200

    refactor: navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -35,6 +35,9 @@ function NavBar() {
             color: 'black',
             display: 'block',
             position: 'relative',
+            fontSize: { md: '1.1rem' },
+            padding: { md: '10px 15px' },
+            fontWeight: 'bold',
             '&:hover::after': {
               content: '""',
               position: 'absolute',
@@ -45,7 +48,6 @@ function NavBar() {
               height: '4px',
               backgroundColor: 'primary.main',
             },
-            // Aplicar estilo si el enlace es el activo
             '&::after': location.pathname === path ? {
               content: '""',
               position: 'absolute',
@@ -66,84 +68,89 @@ function NavBar() {
 
   return (
     <AppBar position='static' sx={{ width: '100%', bgcolor: 'transparent', boxShadow: 'none' }}>
-      <Toolbar disableGutters sx={{ px: 2 }}>
-        {/* Logo */}
-        <Typography
-          variant='h6'
-          noWrap
-          component='div'
-          sx={{
-            mr: 2,
-            display: { xs: 'none', md: 'flex' },
-            fontFamily: 'monospace',
-            fontWeight: 700,
-            letterSpacing: '.3rem',
-            color: 'primary.main',
-            textDecoration: 'none',
-          }}>
-          LAS NAVES
-        </Typography>
+      <Toolbar disableGutters sx={{ px: 2, height: { md: '80px' } }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', width: '100%' }}>
+          <Typography
+            variant='h6'
+            noWrap
+            component='div'
+            sx={{
+              display: { xs: 'none', md: 'flex', xl: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 900,
+              fontSize: '1.5rem',
+              letterSpacing: '.3rem',
+              color: 'primary.main',
+              textDecoration: 'none',
+              marginLeft: { md: '10%' },
+              flexGrow: 1,
+            }}>
+            LAS NAVES
+          </Typography>
 
-        {/* Mobile Menu */}
-        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-          <IconButton
-            size='large'
-            aria-label='open menu'
-            onClick={handleOpenNavMenu}
-            color='primary'>
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            id='menu-appbar'
-            anchorEl={anchorElNav}
-            open={Boolean(anchorElNav)}
-            onClose={handleCloseNavMenu}
-            anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-            transformOrigin={{ vertical: 'top', horizontal: 'left' }}>
-            {[
-              <MenuItem key="home" onClick={handleCloseNavMenu}>{renderNavLink('Home', '/')}</MenuItem>,
-              <MenuItem key="rooms" onClick={handleCloseNavMenu}>{renderNavLink('Rooms', '/rooms')}</MenuItem>,
-              !isLoggedIn && <MenuItem key="login" onClick={handleCloseNavMenu}>{renderNavLink('Login', '/login')}</MenuItem>,
-              !isLoggedIn && <MenuItem key="register" onClick={handleCloseNavMenu}>{renderNavLink('Register', '/register')}</MenuItem>,
-              isLoggedIn && <MenuItem key="profile" onClick={handleCloseNavMenu}>{renderNavLink('Profile', '/profile')}</MenuItem>,
-              isLoggedIn && isAdmin && <MenuItem key="admin" onClick={handleCloseNavMenu}>{renderNavLink('Admin Dashboard', '/admin')}</MenuItem>,
-              isLoggedIn && <MenuItem key="logout" onClick={handleLogout}>Logout</MenuItem>,
-            ]}
-          </Menu>
-        </Box>
+          {/* Mobile Menu */}
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size='large'
+              aria-label='open menu'
+              onClick={handleOpenNavMenu}
+              color='primary'>
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id='menu-appbar'
+              anchorEl={anchorElNav}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+              transformOrigin={{ vertical: 'top', horizontal: 'left' }}>
+              {[
+                <MenuItem key="home" onClick={handleCloseNavMenu}>{renderNavLink('Home', '/')}</MenuItem>,
+                <MenuItem key="rooms" onClick={handleCloseNavMenu}>{renderNavLink('Rooms', '/rooms')}</MenuItem>,
+                !isLoggedIn && <MenuItem key="login" onClick={handleCloseNavMenu}>{renderNavLink('Login', '/login')}</MenuItem>,
+                !isLoggedIn && <MenuItem key="register" onClick={handleCloseNavMenu}>{renderNavLink('Register', '/register')}</MenuItem>,
+                isLoggedIn && <MenuItem key="profile" onClick={handleCloseNavMenu}>{renderNavLink('Profile', '/profile')}</MenuItem>,
+                isLoggedIn && isAdmin && <MenuItem key="admin" onClick={handleCloseNavMenu}>{renderNavLink('Admin Dashboard', '/admin')}</MenuItem>,
+                isLoggedIn && <MenuItem key="logout" onClick={handleLogout}>Logout</MenuItem>,
+              ]}
+            </Menu>
+          </Box>
 
-        {/* Desktop Menu */}
-        <Typography
-          variant='h5'
-          noWrap
-          component='div'
-          sx={{
-            mr: 2,
-            display: { xs: 'flex', md: 'none' },
-            flexGrow: 1,
-            fontFamily: 'monospace',
-            fontWeight: 700,
-            letterSpacing: '.3rem',
-            color: 'primary.main',
-            textDecoration: 'none',
-          }}>
-          LAS NAVES
-        </Typography>
-        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-          {renderNavLink('Home', '/')}
-          {renderNavLink('Rooms', '/rooms')}
-          {!isLoggedIn ? (
-            <>
-              {renderNavLink('Login', '/login')}
-              {renderNavLink('Register', '/register')}
-            </>
-          ) : (
-            <>
-              {renderNavLink('Profile', '/profile')}
-              {isAdmin && renderNavLink('Admin Dashboard', '/admin')}
-              <Button onClick={handleLogout}>Logout</Button>
-            </>
-          )}
+          {/* Mobile Logo */}
+          <Typography
+            variant='h5'
+            noWrap
+            component='div'
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 900,
+              letterSpacing: '.3rem',
+              color: 'primary.main',
+              textDecoration: 'none',
+            }}>
+            LAS NAVES
+          </Typography>
+
+          {/* Desktop Menu */}
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', xl: 'flex' }, justifyContent: 'center' }}>
+            {renderNavLink('Home', '/')}
+            {renderNavLink('Rooms', '/rooms')}
+            {!isLoggedIn ? (
+              <>
+                {renderNavLink('Login', '/login')}
+                {renderNavLink('Register', '/register')}
+              </>
+            ) : (
+              <>
+                {renderNavLink('Profile', '/profile')}
+                {isAdmin && renderNavLink('Admin Dashboard', '/admin')}
+                <Button onClick={handleLogout} sx={{ fontSize: '1.1rem', padding: '10px 15px', fontWeight: 'bold' }}>Logout</Button>
+              </>
+            )}
+          </Box>
         </Box>
       </Toolbar>
     </AppBar>
